e258e4650835fc87f0b298b9acbc41d6e08e7441
Sarah <104779859+SarahNWF@users.noreply.github.com>
2024-05-29 15:24:38 -0400

    Commit full copy of survey
    Survey123 Connect directory

---
+++ survey/f032871677f74a8a940a693187de0ca9/scripts/functions.js
@@ -0,0 +1,150 @@
+function replacefunction(input){
+
+var returnAryA = [];
+var returnAryB = [];
+var returnAryC = [];
+var returnAryD = [];
+var returnAryE = [];
+var returnAryF = [];
+var returnAryG = [];
+var returnAryH = [];
+var returnAryI = [];
+var returnAryJ = [];
+var returnAryK = [];
+var returnAryL = [];
+var returnAryM = [];
+var returnAryN = [];
+var returnAryO = [];
+var returnAryP = [];
+var returnAryQ = [];
+var returnAryR = [];
+var returnAryS = [];
+var returnAryT = [];
+var returnAryU = [];
+var returnAryV = [];
+var returnAryW = [];
+var returnAryX = [];
+var returnAryY = [];
+var returnAryZ = [];
+var returnAryunder = [];
+var returnAryend = [];
+
+for (var i = 0; i < input.length; i++) {
+  var replace = input[i].replace("A" , " A");
+  returnAryA.push(replace);
+}
+for (var i = 0; i < returnAryA.length; i++) {
+  var replace = returnAryA[i].replace("B" , " B");
+  returnAryB.push(replace);
+}
+for (var i = 0; i < returnAryB.length; i++) {
+  var replace = returnAryB[i].replace("C", " C");
+  returnAryC.push(replace);
+}
+for (var i = 0; i < returnAryC.length; i++) {
+  var replace = returnAryC[i].replace("D", " D");
+  returnAryD.push(replace);
+}
+for (var i = 0; i < returnAryD.length; i++) {
+  var replace = returnAryD[i].replace("E", " E");
+  returnAryE.push(replace);
+}
+for (var i = 0; i < returnAryE.length; i++) {
+  var replace = returnAryE[i].replace("F", " F");
+  returnAryF.push(replace);
+}
+for (var i = 0; i < returnAryF.length; i++) {
+  var replace = returnAryF[i].replace("G", " G");
+  returnAryG.push(replace);
+}
+for (var i = 0; i < returnAryG.length; i++) {
+  var replace = returnAryG[i].replace("H", " H");
+  returnAryH.push(replace);
+}
+for (var i = 0; i < returnAryH.length; i++) {
+  var replace = returnAryH[i].replace("I", " I");
+  returnAryI.push(replace);
+}
+for (var i = 0; i < returnAryI.length; i++) {
+  var replace = returnAryI[i].replace("J", " J");
+  returnAryJ.push(replace);
+}
+for (var i = 0; i < returnAryJ.length; i++) {
+  var replace = returnAryJ[i].replace("K", " K");
+  returnAryK.push(replace);
+}
+for (var i = 0; i < returnAryK.length; i++) {
+  var replace = returnAryK[i].replace("L", " L");
+  returnAryL.push(replace);
+}
+for (var i = 0; i < returnAryL.length; i++) {
+  var replace = returnAryL[i].replace("M", " M");
+  returnAryM.push(replace);
+}
+for (var i = 0; i < returnAryM.length; i++) {
+  var replace = returnAryM[i].replace("N", " N");
+  returnAryN.push(replace);
+}
+for (var i = 0; i < returnAryN.length; i++) {
+  var replace = returnAryN[i].replace("O", " O");
+  returnAryO.push(replace);
+}
+for (var i = 0; i < returnAryO.length; i++) {
+  var replace = returnAryO[i].replace("P", " P");
+  returnAryP.push(replace);
+}
+for (var i = 0; i < returnAryP.length; i++) {
+  var replace = returnAryP[i].replace("Q", " Q");
+  returnAryQ.push(replace);
+}
+for (var i = 0; i < returnAryQ.length; i++) {
+  var replace = returnAryQ[i].replace("R", " R");
+  returnAryR.push(replace);
+}
+for (var i = 0; i < returnAryR.length; i++) {
+  var replace = returnAryR[i].replace("S", " S");
+  returnAryS.push(replace);
+}
+for (var i = 0; i < returnAryS.length; i++) {
+  var replace = returnAryS[i].replace("T", " T");
+  returnAryT.push(replace);
+}
+for (var i = 0; i < returnAryT.length; i++) {
+  var replace = returnAryT[i].replace("U", " U");
+  returnAryU.push(replace);
+}
+for (var i = 0; i < returnAryU.length; i++) {
+  var replace = returnAryU[i].replace("V", " V");
+  returnAryV.push(replace);
+}
+for (var i = 0; i < returnAryV.length; i++) {
+  var replace = returnAryV[i].replace("W", " W");
+  returnAryW.push(replace);
+}
+for (var i = 0; i < returnAryW.length; i++) {
+  var replace = returnAryW[i].replace("X", " X");
+  returnAryX.push(replace);
+}
+for (var i = 0; i < returnAryX.length; i++) {
+  var replace = returnAryX[i].replace("Y", " Y");
+  returnAryY.push(replace);
+}
+for (var i = 0; i < returnAryY.length; i++) {
+  var replace = returnAryY[i].replace("Z", " Z");
+  returnAryZ.push(replace);
+}
+for (var i = 0; i < returnAryZ.length; i++) {
+  var replace = returnAryZ[i].replace("_", "");
+  returnAryunder.push(replace);
+}
+for (var i = 0; i < returnAryunder.length; i++) {
+	if (i == 0){
+		var replace = returnAryunder[i].replace(" ", "");
+  		returnAryend.push(replace);
+	}
+	else {
+		returnAryend.push(returnAryunder[i]);
+	}
+}
+return returnAryend;
+}
